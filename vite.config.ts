81bea410f599048f0222384e6fc2c0a325bd236d
Chum_mul <40909985+waterfirst@users.noreply.github.com>
2025-02-27 23:58:43 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bolt_app/', // 저장소 이름 추가
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
